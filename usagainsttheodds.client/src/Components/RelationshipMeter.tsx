d04121359a5e86bd179bb348dd76e1779a130de0
likes <likes@LVEDRALPC>
2025-11-25 18:56:35 +0100

    started: relatiopnship meter

---
+++ usagainsttheodds.client/src/Components/RelationshipMeter.tsx
@@ -0,0 +1,19 @@
+import type React from "react"
+
+type RelationShipMeterProps = {
+    relationshipValue: number
+}
+
+
+
+const RelationShipMeter:React.FC<RelationShipMeterProps> = ({relationshipValue}) => {
+
+    //doslo mi ze se mi to nechce delat tak kdybyste nekdo meli naladu tak pls
+    
+
+  return (
+    <div>
+
+    </div>
+  )
+}
