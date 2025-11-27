b95c192d0755b0d0e0fe6a041990ec1167b812aa
vojjtech <vojtech.platek.023@pslib.cz>
2025-11-27 23:24:19 +0100

    Edit: fix linků

--- usagainsttheodds.client/src/Pages/Minigames/RussianRulette.tsx
+++ usagainsttheodds.client/src/Pages/Minigames/RussianRulette.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import MinigameInfo from "../Components/MinigameInfo"
-import type { Screen } from "../Types/GameType"
-import { useMinigameExit } from "../hooks/useMinigameExit"
-import GameEnd from "../Components/GameEnd";
-import MiniGamePreset from "../Components/MinigamePreset";
+import MinigameInfo from "../../Components/MinigameInfo"
+import type { Screen } from "../../Types/GameType"
+import { useMinigameExit } from "../../hooks/useMinigameExit"
+import GameEnd from "../../Components/GameEnd";
+import MiniGamePreset from "../../Components/MinigamePreset";
 
 type RussianRuletteProps = {
     setCurrentScreen: (screen: Screen) => void
