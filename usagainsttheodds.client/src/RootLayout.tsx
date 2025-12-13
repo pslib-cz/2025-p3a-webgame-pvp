357d0031fbbcd893e0130359976dadfbe747a6cb
Nella Pražáková <nella.prazakova.023@pslib.cz>
2025-12-13 14:37:46 +0100

    add: routing, změna struktury projektu a přidán rootlayoutu

---
+++ usagainsttheodds.client/src/RootLayout.tsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from 'react';
+import HUD from './Components/HUD/HUD';
+import {type UserData } from './Types/UserDataType';
+import { Outlet } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
+
+const RootLayout = () => {
+
+  const [userName, setUserName] = useState<string>("John");
+  const navigate = useNavigate();
+
+
+  
+  const intitialData: UserData = {
+    ticketsAmount: 50,
+    relationshipStamina: 85,
+    playerName: userName,
+    //currentPage: null, BUDEME UKLÁDÁT SPÍŠ POZICI HRÁČE NA X, ALE ZATIM DELAM ZE TO NEVIDIM
+    boughtBloon: false,
+    boughtFlower: false,
+    lastDrink: undefined,
+    lastFood: undefined
+  }
+
+
+
+  const [userData, setUserData] = useState<UserData>(() => {
+
+      const stored = localStorage.getItem("UserData");
+      return stored ? JSON.parse(stored) : intitialData;  // Return saved data
+  });//taha data z local storage jinak hodi initial value
+
+
+
+  
+  const [tickets, setTickets] = useState<number>(userData.ticketsAmount);
+  const [relationshipValue, setRelationshipValue] = useState(userData.relationshipStamina);
+  
+
+  useEffect(() => {
+      const updated = {
+        ...userData,
+        ticketsAmount: tickets,
+        relationshipStamina: relationshipValue,
+      };
+      localStorage.setItem("UserData", JSON.stringify(updated));
+
+
+    setUserData(prev => {
+      if (
+        prev.ticketsAmount === updated.ticketsAmount &&
+        prev.relationshipStamina === updated.relationshipStamina
+      ) {
+        return prev; // nic se nezměnilo
+      }
+      return updated;
+    });
+  }, [tickets, relationshipValue, userData]);//ulozi do local storage kdyz se zmeni hodnota
+
+  return (
+    <div className="game-root">
+
+      {/*to co cheme ukládat pořád*/}
+      <HUD tickets={tickets} relationship={relationshipValue} />
+
+      {/* tady se jakoby vykresluje ta page co chci? */}
+      <Outlet
+        context={{
+          tickets,
+          setTickets,
+          relationshipValue,
+          setRelationshipValue,
+        }}
+      />
+
+    </div>
+
+/*
+  ---pro preklikavani page
+    <button className="building" onClick={() => navigate("/blackjack")}>
+      Blackjack
+    </button>
+
+
+    ---braní dat např. v mnihrách se dělá takto:
+    const { tickets } = useOutletContext<{
+      tickets: number;
+    }>();
+    */
+     
+
+  );
+  
+}
+
+export default RootLayout;
--- usagainsttheodds.client/src/main.tsx
+++ usagainsttheodds.client/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Blackjack from './Pages/Minigames/Blackjack.tsx';
+import RussianRulette from './Pages/Minigames/RussianRulette.tsx';
+import NotFoundPage from './Pages/NotFoundPage.tsx';
+import RootLayout from './RootLayout.tsx';
+import HomePage from './Pages/HomePage.tsx';
+import ErrorPage from './Pages/ErrorPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+        <Route index element={<HomePage />} />
+
+        {/* minihry */}
+        <Route path="blackjack" element={<Blackjack />} />
+        <Route path="russianroulette" element={<RussianRulette />} />
+
+        {/* 404 */}
+        <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
