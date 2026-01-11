c8beba7a461b50800627525d9c0793b055f44b44
Nella Pražáková <nella.prazakova.023@pslib.cz>
2026-01-11 18:55:24 +0100

    edit: startpage

--- usagainsttheodds.client/src/Pages/StartPage.tsx
+++ usagainsttheodds.client/src/Pages/StartPage.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import styles from "../assets/styles/Intro.module.css"
 
 const StartPage = () => {
     const navigate = useNavigate();
@@ -16,17 +17,21 @@ const StartPage = () => {
     };
 
     return (
-        <>
+        <div className={styles.startpage}>
+            <div className={styles.text}>
             <h1>Us against the odds</h1>
-            <button className="button-intro" onClick={NewGame}>
+            <div className={styles.buttons}>
+            <button className={styles.buttonIntro} onClick={NewGame}>
                 New game
             </button>
             {hasSave && (
-            <button className="button-intro" onClick={Continue}>
+            <button className={styles.buttonIntro} onClick={Continue}>
                 Continue
             </button>
             )}
-        </>
+            </div>
+            </div>
+        </div>
     );
 };
 export default StartPage;
