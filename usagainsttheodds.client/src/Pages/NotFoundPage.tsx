fb7a958105a6be664629adda94db5e415933c12f
lukas.vedral <lukas.vedral.023@pslib.cz>
2026-01-14 22:49:05 +0100

    lunch break 404

--- usagainsttheodds.client/src/Pages/NotFoundPage.tsx
+++ usagainsttheodds.client/src/Pages/NotFoundPage.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
   <div>
-    <h1>404: Stránka nenalezena</h1>
-    <p>Tahle cesta neexistuje. Zkus to jinak!</p>
-    <Link to="/game">Zpět na hrací plochu.</Link>
+    <h1>404: Lunch break</h1>
+    <p>Give them a rest, go try other minigames.</p>
+    <Link to="/game">Back to main screen.</Link>
   </div>
 );
 
