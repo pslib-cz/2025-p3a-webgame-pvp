49c8e776b1705626cce39419e40e4958ba23f9aa
Nella Pražáková <nella.prazakova.023@pslib.cz>
2026-01-22 22:49:24 +0100

    LIKES koukni do introrules pls

--- usagainsttheodds.client/src/Pages/StartPage.tsx
+++ usagainsttheodds.client/src/Pages/StartPage.tsx
@@ -4,10 +4,16 @@ import { Outlet } from "react-router-dom";
 import styles from "../assets/styles/Intro.module.css"
 import { useOwnOutlet } from '../Hooks/useOwnOutlet';
 import ResetButton from '../Components/ResetButton';
+import IntroRules from '../Components/IntroRules';
 
 const StartPage = () => {
     const navigate = useNavigate();
     const hasSave = localStorage.getItem("UserData");
+    const [showComponent, setShowComponent] = useState(false);
+
+    const handleShow = () => {
+        setShowComponent(true);
+    };
 
     const NewGame = () => {
         localStorage.removeItem("UserData");
@@ -22,16 +28,17 @@ const StartPage = () => {
     return (
         <div className={styles.startpage}>
             <div className={styles.text}>
-            <h1>Us against the odds</h1>
-            <div className={styles.buttons}>
-            <ResetButton navigateTo="/cutscene/intro" className={styles.buttonIntro} text='New Game'/>
-            {hasSave && (
-            <button className={styles.buttonIntro} onClick={Continue}>
-                Continue
-            </button>
-            )}
+                <h1>Us against the odds</h1>
+                <div className={styles.buttons}>
+                    <button className={styles.buttonIntro} onClick={handleShow}>New game</button>
+                    {hasSave && (
+                    <button className={styles.buttonIntro} onClick={Continue}>
+                        Continue
+                    </button>
+                    )}
+                </div>
             </div>
-            </div>
+            {showComponent && <IntroRules />}
         </div>
     );
 };
