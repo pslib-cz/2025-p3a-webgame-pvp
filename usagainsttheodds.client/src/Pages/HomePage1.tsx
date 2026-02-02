8299e84293ebc618a1a960e5ec7a9b46cab4e3e4
Nella Pražáková <nella.prazakova.023@pslib.cz>
2026-02-02 14:20:19 +0100

    edit: building styles

--- usagainsttheodds.client/src/Pages/HomePage1.tsx
+++ usagainsttheodds.client/src/Pages/HomePage1.tsx
@@ -13,28 +13,19 @@ const HomePage1 = () => {
       <div className={styles.background} />
 
       <div className={styles.main_right}>
-        <div
-          className={styles.goLEFT_btn}
-          onClick={() => navigate("/game/right")}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="25" viewBox="0 0 57 60" fill="none">
-            <path d="M47.8285 32.8284C49.3906 31.2663 49.3906 28.7337 47.8285 27.1716L22.3727 1.71573C20.8106 0.153632 18.2779 0.153632 16.7158 1.71573C15.1537 3.27783 15.1537 5.81049 16.7158 7.37258L39.3432 30L16.7158 52.6274C15.1537 54.1895 15.1537 56.7222 16.7158 58.2843C18.2779 59.8464 20.8106 59.8464 22.3727 58.2843L47.8285 32.8284ZM45 30V34H45.0001V30V26H45V30Z" fill="#131522" />
-          </svg>
-        </div>
+        <div className={styles.goLEFT_btn} onClick={() => navigate("/game/right")}/>
 
         <div className={styles.games}>
-          
 
           <button className={`${styles.building} ${styles.wheel}`} onClick={() => navigate("/wheel")} />
           <button className={`${styles.building} ${styles.shop}`} onClick={() => navigate("/itemshop")} />
           <button className={`${styles.building} ${styles.feelinglucky}`} onClick={() => navigate("/minigame/feelinglucky")} /> {/* nema routu udelanou*/}
           <button className={`${styles.building} ${styles.russianroulette}`} onClick={() => navigate("/minigame/russianroulette")} />
-          <button className={`${styles.building} ${styles.baloons}`} onClick={() => navigate("/minigame/baloons")} /> {/* nema routu udelanou*/}
           <button className={`${styles.building} ${styles.darts}`} onClick={() => navigate("/minigame/darts")} />
-          {/* <Russianroulette/> */}
-        </div>
 
+        </div>
         <div className={styles.NPC}>
+          <span className={`${styles.npc_3} ${styles.baloons}`}></span>
           <span className={`${styles.npc_1} ${styles.family}`}></span>
         </div>
 
--- usagainsttheodds.client/src/Pages/HomePage2.tsx
+++ usagainsttheodds.client/src/Pages/HomePage2.tsx
@@ -19,9 +19,6 @@ const HomePage2 = () => {
           className={styles.goRIGHT_btn}
           onClick={() => navigate("/game")}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="25" viewBox="0 0 57 60" fill="none">
-            <path d="M47.8285 32.8284C49.3906 31.2663 49.3906 28.7337 47.8285 27.1716L22.3727 1.71573C20.8106 0.153632 18.2779 0.153632 16.7158 1.71573C15.1537 3.27783 15.1537 5.81049 16.7158 7.37258L39.3432 30L16.7158 52.6274C15.1537 54.1895 15.1537 56.7222 16.7158 58.2843C18.2779 59.8464 20.8106 59.8464 22.3727 58.2843L47.8285 32.8284ZM45 30V34H45.0001V30V26H45V30Z" fill="#131522" />
-          </svg>
         </div>
 
         <div className={styles.games}>
@@ -29,8 +26,7 @@ const HomePage2 = () => {
           <button className={`${styles.building} ${styles.slots}`} onClick={() => navigate("/minigame/slots")} />
           <button className={`${styles.building} ${styles.blackjack}`} onClick={() => navigate("/minigame/blackjack")} />
           <button className={`${styles.building} ${styles.whackamole}`} onClick={() => navigate("/minigame/whackamole")} />
-          <button className={`${styles.building} ${styles.pexeso}`} onClick={() => navigate("/minigame/memorymatch")} /> {/* nema routu udelanou*/}
-          {/* nema routu udelanou*/}
+          <button className={`${styles.building} ${styles.pexeso}`} onClick={() => navigate("/minigame/memorymatch")} />
         </div>
 
         <div className={styles.NPC}>
