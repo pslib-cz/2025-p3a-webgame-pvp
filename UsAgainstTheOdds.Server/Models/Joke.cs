f4a12ad4c05aebd75973bb85acff082a2996ec8c
vojjtech <vojtech.platek.023@pslib.cz>
2026-02-02 14:09:50 +0100

    d

--- UsAgainstTheOdds.Server/Models/Joke.cs
+++ UsAgainstTheOdds.Server/Models/Joke.cs
@@ -2,11 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UsAgainstTheOdds.Server.Models
 {
-    public class Ending
+    public class Joke
     {
         [Key]
         public int JokeId { get; set; }
-        public required string Joke{ get; set; }
+        public required string JokeText{ get; set; }
         public required string Punchline { get; set; }
         public bool IsFunny { get; set; }
     }
